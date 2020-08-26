603834cbac2d2921639dd7fdd94768143b1ed9f5
John Welsh <jwelsh@nvidia.com>
2020-08-26 00:41:02 -0700

    added hardware accelerated jpeg encoder

--- jetbot/image.py
+++ jetbot/image.py
@@ -1,6 +1,14 @@
 import enum
 import cv2
+from .jpeg_encoder import JpegEncoder
 
 
-def bgr8_to_jpeg(value, quality=75):
+_ENCODER = JpegEncoder(width=224, height=224, fps=21)
+
+
+def bgr8_to_jpeg(value):
+    return _ENCODER.encode(value)
+
+
+def bgr8_to_jpeg_legacy(value, quality=75):
     return bytes(cv2.imencode('.jpg', value)[1])
