c5c5c5cdf0789ac03ff9d7c1998895cdf9115bbb
John Welsh <jwelsh@nvidia.com>
2020-08-27 18:39:53 -0700

    fixed asc path

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,4 +1,4 @@
-cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc ./
+cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc ../.. # copy to jetbot root
 
 sudo docker build \
     -t jetbot-base:$JETBOT_VERSION \
