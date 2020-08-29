d1593b5a870aff88a1ab1b0607dc87f9d5185931
John Welsh <jwelsh@nvidia.com>
2020-08-28 18:48:24 -0700

    made zmq camera default

--- jetbot/camera/camera.py
+++ jetbot/camera/camera.py
@@ -6,12 +6,10 @@ class Camera(traitlets.HasTraits):
     
     value = traitlets.Any()
     
-    DEFAULT_CAMERA_CLASS = 'OpenCvGstCamera'
-    
     @staticmethod
     def default_camera_class():
-        from .opencv_gst_camera import OpenCvGstCamera
-        return OpenCvGstCamera
+        from .zmq_camera import ZmqCamera
+        return ZmqCamera
     
     @staticmethod
     def instance(*args, **kwargs):
