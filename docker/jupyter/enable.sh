293398ca64780c2b47acdf93968e68a86436087a
John Welsh <jwelsh@nvidia.com>
2020-09-21 17:57:19 -0700

    added memory limit configuration

--- docker/jupyter/enable.sh
+++ docker/jupyter/enable.sh
@@ -1,17 +1,48 @@
 WORKSPACE=$1
 JETBOT_CAMERA={$2:-opencv_gst_camera}
 
-sudo docker run -it -d \
-    --restart always \
-    --runtime nvidia \
-    --network host \
-    --privileged \
-    --device /dev/video* \
-    --volume /dev/bus/usb:/dev/bus/usb \
-    --volume /tmp/argus_socket:/tmp/argus_socket \
-    -p 8888:8888 \
-    -v $WORKSPACE:/workspace \
-    --workdir /workspace \
-    --name=jetbot_jupyter \
-    --env JETBOT_DEFAULT_CAMERA=$JETBOT_CAMERA \
-    jetbot/jetbot:jupyter-$JETBOT_VERSION-$L4T_VERSION
+# set default swap limit as unlimited
+if [[ -z "$JETBOT_JUPYTER_MEMORY_SWAP" ]]
+then
+	export JETBOT_JUPYTER_MEMORY_SWAP=-1
+fi
+
+if [[ -z "$JETBOT_JUPYTER_MEMORY" ]]
+then
+
+	sudo docker run -it -d \
+	    --restart always \
+	    --runtime nvidia \
+	    --network host \
+	    --privileged \
+	    --device /dev/video* \
+	    --volume /dev/bus/usb:/dev/bus/usb \
+	    --volume /tmp/argus_socket:/tmp/argus_socket \
+	    -p 8888:8888 \
+	    -v $WORKSPACE:/workspace \
+	    --workdir /workspace \
+	    --name=jetbot_jupyter \
+	    --memory-swap=$JETBOT_JUPYTER_MEMORY_SWAP \
+	    --env JETBOT_DEFAULT_CAMERA=$JETBOT_CAMERA \
+	    jetbot/jetbot:jupyter-$JETBOT_VERSION-$L4T_VERSION
+
+else
+
+	sudo docker run -it -d \
+	    --restart always \
+	    --runtime nvidia \
+	    --network host \
+	    --privileged \
+	    --device /dev/video* \
+	    --volume /dev/bus/usb:/dev/bus/usb \
+	    --volume /tmp/argus_socket:/tmp/argus_socket \
+	    -p 8888:8888 \
+	    -v $WORKSPACE:/workspace \
+	    --workdir /workspace \
+	    --name=jetbot_jupyter \
+	    --memory=$JETBOT_JUPYTER_MEMORY \
+	    --memory-swap=$JETBOT_JUPYTER_MEMORY_SWAP \
+	    --env JETBOT_DEFAULT_CAMERA=$JETBOT_CAMERA \
+	    jetbot/jetbot:jupyter-$JETBOT_VERSION-$L4T_VERSION
+
+fi
