d2faa17e4c8014ce73f567c696e5eea5ce2c7772
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2021-06-29 09:16:59 -0600

    Add support for new Jetpack images
    Add a compatible base image, as mentioned in this issue: https://github.com/NVIDIA-AI-IOT/jetbot/issues/406#issuecomment-837006950
    
    Works with latest pre-built images.

--- docker/configure.sh
+++ docker/configure.sh
@@ -15,6 +15,10 @@ then
 elif [[ "$L4T_VERSION" == "32.4.4" ]]
 then
 	export JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3
+elif [[ "$L4T_VERSION" == "32.5.0" ]]
+then
+    export JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.6-py3
+	# export JETBOT_BASE_IMAGE=nvcr.io/ea-linux4tegra/l4t-pytorch:r32.5.0-pth1.7-py3	# from bash history of pre-built 2GB image: jetbot-043_nano-2gb-jp45.zip
 else
 	echo "JETBOT_BASE_IMAGE not found for ${L4T_VERSION}.  Please manually set the JETBOT_BASE_IMAGE environment variable. (ie: export JETBOT_BASE_IMAGE=...)"
 fi
