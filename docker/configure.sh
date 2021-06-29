cd68ab628d8bdbc19a11fa2ed6bcb20950f06886
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2021-06-29 08:54:33 -0600

    Use environment variable
    Move **BASE_IMAGE** configuration from /base `Dockerfile` to `configure.sh` script:
    - Set **JETBOT_BASE_IMAGE** as an environment variable
    - Pass variable to /base `Dockerfile`

--- docker/configure.sh
+++ docker/configure.sh
@@ -11,10 +11,10 @@ export L4T_VERSION="$L4T_RELEASE.$L4T_REVISION"
 
 if [[ $L4T_VERSION = "32.4.3" ]]
 then
-	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.3-pth1.6-py3
+	export JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.3-pth1.6-py3
 elif [[ "$L4T_VERSION" == "32.4.4" ]]
 then
-	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3
+	export JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3
 else
 	echo "JETBOT_BASE_IMAGE not found for ${L4T_VERSION}.  Please manually set the JETBOT_BASE_IMAGE environment variable. (ie: export JETBOT_BASE_IMAGE=...)"
 fi
